e6e872ddcafe8f1149365c65e8869ab57c221767
klemmons72 <67609804+klemmons72@users.noreply.github.com>
2022-03-31 13:04:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function proxyRotator(){
 let ip_addresses = [];
 let port_numbers = [];
 
-request("https://api.coolproxies.com/api.php?list=all&http=1&apikey=IEJQPS82K2U8888A", function(error, response, html) {
+request("http://sslproxies.com/", function(error, response, html) {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
 
